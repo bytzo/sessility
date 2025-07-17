67561ff16e9b370f880f3cb4a5ae20cbcaf690d1
Bytzo <bytzo@bytzo.net>
2025-07-17 10:17:15 -0400

    Update to Minecraft 1.21.8

--- fabric-mod/build.gradle.kts
+++ fabric-mod/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-	minecraft("com.mojang:minecraft:1.21.7")
+	minecraft("com.mojang:minecraft:1.21.8")
 	mappings(loom.officialMojangMappings())
 	modImplementation("net.fabricmc:fabric-loader:0.16.14")
 }
